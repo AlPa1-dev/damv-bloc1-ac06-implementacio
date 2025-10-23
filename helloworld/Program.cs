572028530c1b2c99b8660f20bf099fe80e101e59
gerard <gerard.alonso.7e9@itb.cat>
2025-10-23 09:56:28 +0200

    exercise0

---
+++ helloworld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace HelloWorldProject
+{
+    public class HelloWorld
+    {
+        public static void Main()
+        {
+            const string Msg = "Introdueix el teu nom: ";
+            string name;
+            Console.WriteLine(Msg);
+            name = Console.ReadLine();
+            Console.WriteLine("Hola, " + name + "!");
+        }
+    }
+}
